3c21fd1edf920740503583125db98de3ca618ff5
Edson Finda <efinda@c1r3s6.42luanda.com>
2024-12-16 01:11:53 +0100

    Testing also resize and resize_and_fill

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 07:11:36 by efinda            #+#    #+#             */
-/*   Updated: 2024/12/16 00:15:23 by efinda           ###   ########.fr       */
+/*   Updated: 2024/12/16 00:29:26 by efinda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,25 @@
 
 int	main(int ac, char **av)
 {
-	if (ac != 2)
-		return (printf("At least an argument is required\n"));
+	if (ac != 4)
+	{
+		printf("Input 3 args: str, n, and c\n");
+		return (1);
+	}
 	t_string	str;
+	size_t	n = ft_atoi(av[2]);
+	char	c = *av[3];
+	
+	printf("The args inputed are: %s\t%ld\t%c\n", av[1], n, c);
 
 	str.constructor = &constructor;
 	str.constructor(&str, ft_strdup(av[1]));
+
+	str.resize(&str, n);
+	resize_and_fill(&str, n, c);
+	
+	printf("The str became '%s', and it has %ld chars\n", str.buffer, str.len(&str));
 	str.clear(&str);
-	printf("%s has %ld chars\n", str.buffer, str.len(&str));
 	str.destructor(&str);
 	return (0);
 }
