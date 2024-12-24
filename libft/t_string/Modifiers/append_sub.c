5081c76694607b4e4515cd1e6dbc290231fcd243
efinda <168301365+3dsonnn@users.noreply.github.com>
2024-12-24 21:10:30 +0000

    added the last check on the first if condition

--- libft/t_string/Modifiers/append_sub.c
+++ libft/t_string/Modifiers/append_sub.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   append_sub.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: efinda <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: codespace <codespace@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 19:58:44 by efinda            #+#    #+#             */
-/*   Updated: 2024/12/19 19:28:59 by efinda           ###   ########.fr       */
+/*   Updated: 2024/12/24 21:09:09 by codespace        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	append_sub(t_string *str, t_string *s, size_t subpos, size_t sublen)
 {
-    if (!str || !s || !s->buffer || s->empty(s) || subpos >= s->len(s))
+    if (!str || !s || !s->buffer || s->empty(s) || subpos >= s->len(s) || sublen == 0)
         return ;
     s->tmp = ft_substr(s->buffer, subpos, sublen);
     if (!str->buffer)
